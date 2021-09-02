860e5f4e85285f98773ee1311fb5b14af713a0f9
Lekapo <lekapo@gmail.com>
2021-09-02 10:16:53 -0300

    Mobile version finished

---
+++ index.js
@@ -0,0 +1,84 @@
+// ******** CHANGE CSS ATTR ON SCROLL ********
+
+const debounce = (fn) => {
+
+    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+    let frame;
+
+    // The debounce function returns a new function that can receive a variable number of arguments
+    return (...params) => {
+
+        // If the frame variable has been defined, clear it now, and queue for next frame
+        if (frame) {
+            cancelAnimationFrame(frame);
+        }
+
+        // Queue our function call for the next frame
+        frame = requestAnimationFrame(() => {
+
+            // Call our function and pass any params we received
+            fn(...params);
+        });
+
+    }
+};
+
+// Reads out the scroll position and stores it in the data attribute
+// so we can use it in our stylesheets
+const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+}
+
+// Listen for new scroll events, here we debounce our `storeScroll` function
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+// Update scroll position for first time
+storeScroll();
+
+// ******** SEARCH OVERLAY ********
+
+
+$(document).ready(function(){
+
+    // document.getElementById('SearchIcon').addEventListener('click', function(){
+    //     $("#searchOverlay").css({ top: '0px' })
+    // });
+
+    document.getElementById('mobileSearchIcon').addEventListener('click', function(){
+
+
+        $("#searchOverlay").css({ top: '0px' })
+
+
+    });
+
+
+    document.getElementById('closeSearchOverlay').addEventListener('click', function(){
+
+
+        $("#searchOverlay").css({ top: '-100px' })
+    });
+
+});
+
+// ******** SWIPER ********
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
---
+++ js/animations.js
@@ -0,0 +1,193 @@
+// ******** ANIMATIONS ********
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.defaults({
+    
+    ease: "Power1.easeInOut",
+    duration: .7, 
+})
+
+ScrollTrigger.defaults({
+    toggleActions: 'play none none reverse',
+    start: 'center 85%',
+    end: 'top 20%',
+    markers: false,
+})
+
+gsap.to(".hero .mobile-logo", {duration:0.6, opacity:1})
+gsap.fromTo(".hero-text-container *", {opacity: 0, x:-100}, {duration: .3, delay: 0.6, opacity: 1, x: 0, stagger: .1, ease: "power2.inOut"})
+
+
+// ******** NAVBAR ********
+
+gsap.fromTo(".navbar", {
+    opacity: 0, y:-200
+    }, {
+        scrollTrigger: {
+            trigger: ".navbar",
+        },
+        opacity: 1, 
+        y: 0, 
+    }
+)
+
+gsap.fromTo(".arrow", {
+    opacity: 0, y:-50
+    }, {
+        scrollTrigger: {
+            trigger: ".navbar",
+        },
+        delay: 1.5,
+        opacity: 1, 
+        y: 0, 
+    }
+)
+
+
+// ******** FIRST SECTION ********
+
+gsap.fromTo("#first-section h2", {
+    opacity: 0, x:-100
+    }, {
+        scrollTrigger: {
+            trigger: "#first-section h2 ",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#first-section video", {
+    opacity: 0, x:100
+    }, {
+        scrollTrigger: {
+            trigger: "#first-section video",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#first-section p", {
+    opacity: 0, x:-100
+    }, {
+        scrollTrigger: {
+            trigger: "#first-section p",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#first-section button", {
+    opacity: 0, x:-100
+    }, {
+        scrollTrigger: {
+            trigger: "#first-section button",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#first-section .home-gallery", {
+    opacity: 0, y:200
+    }, {
+        scrollTrigger: {
+            trigger: "#first-section .home-gallery",
+            start: '-=200 85%',
+        },
+        opacity: 1, 
+        y: 0, 
+    }
+)
+
+
+// ******** SECOND SECTION ********
+
+gsap.fromTo("#second-section h2", {
+    opacity: 0, x:100
+    }, {
+        scrollTrigger: {
+            trigger: "#second-section h2 ",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#second-section img", {
+    opacity: 0, x:-100
+    }, {
+        scrollTrigger: {
+            trigger: "#second-section img",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#second-section p", {
+    opacity: 0, x:100
+    }, {
+        scrollTrigger: {
+            trigger: "#second-section p",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#second-section button", {
+    opacity: 0, x:100
+    }, {
+        scrollTrigger: {
+            trigger: "#second-section button",
+        },
+        opacity: 1, 
+        x: 0, 
+    }
+)
+
+gsap.fromTo("#second-section .home-gallery", {
+    opacity: 0, y:200
+    }, {
+        scrollTrigger: {
+            trigger: "#second-section .home-gallery",
+            start: '-=200 85%',
+        },
+        opacity: 1, 
+        y: 0, 
+    }
+)
+
+
+// ******** CREDITOS ********
+
+gsap.fromTo(".credits h2", {
+    opacity: 0, y:-50
+    }, {
+        scrollTrigger: {
+            trigger: ".credits h2",
+        },
+        opacity: 1, 
+        y: 0,
+    }
+)
+
+
+gsap.fromTo(".credits .swiper-container", {
+    opacity: 0, y:200
+    }, {
+        scrollTrigger: {
+            trigger: ".credits .swiper-container",
+            start: '-=100 85%',
+        },
+        opacity: 1, 
+        y: 0,
+
+
+        ease: "Power1.easeIn",
+        duration: 1, 
+    }
+)
